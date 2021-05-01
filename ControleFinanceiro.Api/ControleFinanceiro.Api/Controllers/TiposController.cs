43a8a20bb5fdf593fc92af7af5aee52c0e96b34b
Mauro Fernando Thomaz de Andrade <nocnoier@hotmail.com>
2021-05-01 10:05:40 -0300

    Adicionar arquivos de projeto.

---
+++ ControleFinanceiro.Api/ControleFinanceiro.Api/Controllers/TiposController.cs
@@ -0,0 +1,30 @@
+﻿using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiposController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public TiposController(Contexto context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tipo>>> GetTipo()
+        {
+            return await _context.Tipos.ToListAsync();
+        }
+    }
+}
